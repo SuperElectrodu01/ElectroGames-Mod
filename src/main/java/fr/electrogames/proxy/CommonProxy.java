bb6c9e40bd386e155001727ed4efb339b6c73533
obelouix <obelouix25@gmail.com>
2016-05-07 13:07:05 +0200

    ajout de common proxy

--- src/main/java/fr/electrogames/proxy/CommonProxy.java
+++ src/main/java/fr/electrogames/proxy/CommonProxy.java
@@ -1,4 +1,4 @@
-package fr.electrogames;
+package fr.electrogames.proxy;
 
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
