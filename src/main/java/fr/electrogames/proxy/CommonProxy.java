f65e3d4f28a421acd5451267c09207170dd430b0
Louis <obelouix@users.noreply.github.com>
2016-05-07 12:48:32 +0200

    ajout des proxy

---
+++ src/main/java/fr/electrogames/proxy/CommonProxy.java
@@ -0,0 +1,26 @@
+package fr.electrogames;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Louis (a.k.a Obelouix) on 07/05/2016.
+ * <br>
+ *  <b>Proxy commun au serveur et au client</b>
+ */
+public class CommonProxy {
+
+    public void preInit(FMLPreInitializationEvent e) {
+
+    }
+
+    public void init(FMLInitializationEvent e) {
+
+    }
+
+    public void postInit(FMLPostInitializationEvent e) {
+
+    }
+
+}
