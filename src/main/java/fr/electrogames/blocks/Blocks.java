f17285256eabbb6f88e70357655dae7e3aee2e00
obelouix <obelouix25@gmail.com>
2016-05-07 13:23:31 +0200

    préparation du block "tablette"

---
+++ src/main/java/fr/electrogames/blocks/Blocks.java
@@ -0,0 +1,14 @@
+package fr.electrogames.blocks;
+
+/**
+ * Created by Louis (a.k.a Obelouix) on 07/05/2016.
+ */
+public class Blocks {
+
+    public static Blocks blockTablette;
+
+
+
+    public static void createBlocks() {
+    }
+}
