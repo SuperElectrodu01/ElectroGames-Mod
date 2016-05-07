a2f7ca3f117a673bfff05c0a81036a7c3a865fe0
obelouix <obelouix25@gmail.com>
2016-05-07 17:30:17 +0200

    ajout block tabouret

--- src/main/java/fr/electrogames/blocks/BlockTablette.java
+++ src/main/java/fr/electrogames/blocks/BlockTablette.java
@@ -2,12 +2,12 @@ package fr.electrogames.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.world.IBlockAccess;
+import net.minecraft.block.state.IBlockState;
 
 /**
  * Created by Louis (a.k.a Obelouix) on 07/05/2016.
  */
-public class BlockTablette extends Block {
+public class BlockTablette extends Block{
 
     public BlockTablette(Material rock) {
         super(rock);
@@ -15,8 +15,6 @@ public class BlockTablette extends Block {
         setResistance(10.0F);
 
     }
-    public boolean shouldSideBeRendered(IBlockAccess par1IBlockAccess, int par2, int par3, int par4, int par5)
-    {
-        return true;
-    }
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {return false;}
 }
